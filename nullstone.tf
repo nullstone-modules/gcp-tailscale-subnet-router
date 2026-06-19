665f6022a302aa4dc1e7b24a77fd79404e7bc612
Brad Sickles <brad.sickles@gmail.com>
2026-06-18 23:50:03 -0400

    NUL-100: upgrade ns provider to ~> 0.11.0 and use gcp_labels
    Replace the lowercased-tags workaround with the new cloud-specific
    data.ns_workspace.this.gcp_labels attribute from terraform-provider-ns
    v0.11.0, and pin the provider to ~> 0.11.0.

--- nullstone.tf
+++ nullstone.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     ns = {
-      source = "nullstone-io/ns"
+      source  = "nullstone-io/ns"
+      version = "~> 0.11.0"
     }
   }
 }
@@ -18,7 +19,7 @@ resource "random_string" "resource_suffix" {
 }
 
 locals {
-  labels        = { for k, v in data.ns_workspace.this.tags : lower(k) => v }
+  labels        = data.ns_workspace.this.gcp_labels
   block_name    = data.ns_workspace.this.block_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
